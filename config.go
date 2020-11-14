2f24a2875b8636ef437cabdc57bde7ed099b24d6
Shekh Ataul <ataul.one@gmail.com>
2020-11-14 19:28:39 +0530

    feat(config): config changes

--- config.go
+++ config.go
@@ -1,27 +1,38 @@
 package sweep
 
 import (
-	"hash"
 	"time"
 )
 
 const (
-	defaultShardsCount = 1000
+	defaultShardsCount = 1024
+
+	defaultShardSize = 4 * 1024 // 4KB
 
 	defaultEntryLifeTime = 10 // minutes
 
 	defaultCleanupInterval = 1 // minute
+
+	defaultMaxEntrySize = 1024 // bytes
 )
 
 type Configuration struct {
 	// ShardsCount represents a fixed number shards sweep will have.
+	// This should be power of two. If it is not, then it will be set
+	// to next power of two greater than current value.
 	ShardsCount int
 
-	// Hasher represents a hash implementation for hashing keys.
-	Hasher hash.Hash64
+	// MaxShardSize represents the upper bound limit of a shard size in bytes.
+	// This should be power of two. If it is not, then it will be set
+	// to next power of two greater than current value.
+	MaxShardSize int
 
-	// EntryLifetime represents lifetime of an entry in the sweep.
+	// EntryLifetime represents lifetime of an Entry in the sweep.
 	EntryLifetime time.Duration
+
+	// MaxEntrySize represents maximum size of Entry in bytes
+	// in sweep can be stored
+	MaxEntrySize int
 
 	// CleanupInterval represents the waiting period between cleanup
 	// cycles in sweep.
