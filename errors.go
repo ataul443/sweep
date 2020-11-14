d78990a59e5c2cf7efb54ad59174e7ea1592a6f7
Shekh Ataul <ataul.one@gmail.com>
2020-11-14 19:33:24 +0530

    feat(error): add ErrEntryTooLarge error

--- errors.go
+++ errors.go
@@ -8,3 +8,7 @@ var ErrClosed = errors.New("sweep closed")
 // ErrEntryNotFound is the error returned when an
 // entry doesn't exist in sweep.
 var ErrEntryNotFound = errors.New("entry not found")
+
+// ErrEntryTooLarge is the error returned when an entry is too large
+// going to be put in sweep.
+var ErrEntryTooLarge = errors.New("entry is too large in size")
